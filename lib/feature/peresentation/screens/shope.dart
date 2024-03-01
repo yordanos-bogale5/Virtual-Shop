76615d396981fd80ff10ab0de2098ed608f1be99
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2024-03-01 21:05:39 +0300

    shope

---
+++ lib/feature/peresentation/screens/shope.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ShopDetailPage extends StatelessWidget {
+  final String shopName;
+  final String description;
+  final List<String> products;
+
+  ShopDetailPage({
+    required this.shopName,
+    required this.description,
+    required this.products,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(shopName),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Description:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            Text(
+              description,
+              style: const TextStyle(fontSize: 16.0),
+            ),
+            const SizedBox(height: 16.0),
+            const Text(
+              'Products:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 8.0),
+            // Display the list of products
+            Expanded(
+              child: ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(products[index]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
